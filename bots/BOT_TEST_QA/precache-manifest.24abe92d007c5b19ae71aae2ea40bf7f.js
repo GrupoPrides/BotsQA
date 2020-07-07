f45a154b6229eefd368508b438afe32a7a38b5b0
GRUPO PRIDES S.A <65557743+GrupoPrides@users.noreply.github.com>
2020-07-07 15:18:43 -0600

    bot QA

---
+++ bots/BOT_TEST_QA/precache-manifest.24abe92d007c5b19ae71aae2ea40bf7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b2835745e1e88856c15a8de4b03cd5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bef4e095162cf336ef31",
+    "url": "./static/css/main.dc1383d8.chunk.css"
+  },
+  {
+    "revision": "1c844fbb72597ffac622",
+    "url": "./static/js/2.3fb14d4e.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "./static/js/2.3fb14d4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bef4e095162cf336ef31",
+    "url": "./static/js/main.196911aa.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "ba4df5336eead862a7dd475ab9175bf2",
+    "url": "./static/media/GP.ba4df533.PNG"
+  }
+]);
