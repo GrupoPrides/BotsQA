00d3ec90354c0c27b34da1a18cc53390b05ca6d0
kchavesv <kchaves@prides.net>
2020-08-25 11:59:15 -0600

    add BOT_PRIDES

---
+++ bots/BOT_PRIDES/precache-manifest.a07ae9808197b2887d8dd6f16db39aa9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ed1cd69127def915ba21a5ca3dc8fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1eea8d05ac65a5c1e141",
+    "url": "./static/css/main.dc1383d8.chunk.css"
+  },
+  {
+    "revision": "1c844fbb72597ffac622",
+    "url": "./static/js/2.3fb14d4e.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "./static/js/2.3fb14d4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1eea8d05ac65a5c1e141",
+    "url": "./static/js/main.e6f10531.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "ba4df5336eead862a7dd475ab9175bf2",
+    "url": "./static/media/GP.ba4df533.PNG"
+  }
+]);
