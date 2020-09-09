0c928939bee2b51bb594451f98e61a8db6bfa881
kchavesv <kchaves@prides.net>
2020-09-08 18:49:30 -0600

    agregar bot imas
    agregar bot del imas

---
+++ bots/BOT_IMAS/precache-manifest.4a6c7859bbfe7edc77f6ebeaa4687f88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3700e8603085e9bf02926055722c3880",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7451104a87d6ab25e25e",
+    "url": "./static/css/main.dc1383d8.chunk.css"
+  },
+  {
+    "revision": "1c844fbb72597ffac622",
+    "url": "./static/js/2.3fb14d4e.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "./static/js/2.3fb14d4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7451104a87d6ab25e25e",
+    "url": "./static/js/main.86da248a.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "ba4df5336eead862a7dd475ab9175bf2",
+    "url": "./static/media/GP.ba4df533.PNG"
+  }
+]);
