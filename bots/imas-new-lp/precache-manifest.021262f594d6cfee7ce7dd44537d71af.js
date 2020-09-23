a7ce4b76dab42997043e278e32d964e0e604440f
kchavesv <kchaves@prides.net>
2020-09-23 14:52:46 -0600

    se agrega nueva etiqueta imas new lp

---
+++ bots/imas-new-lp/precache-manifest.021262f594d6cfee7ce7dd44537d71af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31e40fea27b46a5f661c5d9abee750a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f09865e56a7a0a15d57e",
+    "url": "./static/css/main.1571714f.chunk.css"
+  },
+  {
+    "revision": "6185a1e38ea370fea132",
+    "url": "./static/js/2.156d8caa.chunk.js"
+  },
+  {
+    "revision": "8e43f2e54f2236399d9481f7a7ad185f",
+    "url": "./static/js/2.156d8caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f09865e56a7a0a15d57e",
+    "url": "./static/js/main.fadd41d8.chunk.js"
+  },
+  {
+    "revision": "be959986847417e06759",
+    "url": "./static/js/runtime-main.df6f3a2c.js"
+  },
+  {
+    "revision": "a8fd278b1688dd97f95174b2a4259702",
+    "url": "./static/media/imas index.a8fd278b.PNG"
+  }
+]);
