fba8bf483614050f023b6402ad4f5332236fbd03
Limberth Vasquez Quesada <limberthvasquez@gmail.com>
2020-10-07 16:16:53 -0600

    subir ajuste sandbox

---
+++ bots/LivePersonImasQA/precache-manifest.6072f09883ae6586a777bc2d5b6cfee5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adaed54ff33d8e72e75841eb27414c2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dbf28dccf2d08985c417",
+    "url": "./static/css/main.d12e3aa1.chunk.css"
+  },
+  {
+    "revision": "c00a11bbc7136611f088",
+    "url": "./static/js/2.cd264bca.chunk.js"
+  },
+  {
+    "revision": "4563017d3783b0506315b1de43d1ae90",
+    "url": "./static/js/2.cd264bca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbf28dccf2d08985c417",
+    "url": "./static/js/main.f87e8916.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "b0d36d4de3b0c239bbb182a45044aabb",
+    "url": "./static/media/bg.b0d36d4d.PNG"
+  }
+]);
