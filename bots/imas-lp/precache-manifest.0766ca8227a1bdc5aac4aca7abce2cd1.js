d86efa7e7d1e801660d40467fba82acd9c2c3b36
kchavesv <kchaves@prides.net>
2020-10-28 10:45:05 -0600

    imas cookies

---
+++ bots/imas-lp/precache-manifest.0766ca8227a1bdc5aac4aca7abce2cd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0848325d7f70dcfa511027bfcd540835",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d01a1c4f2fb85a7911f",
+    "url": "./static/css/main.0c5bd5cf.chunk.css"
+  },
+  {
+    "revision": "55705737067598fb3bd1",
+    "url": "./static/js/2.d5cd9cf9.chunk.js"
+  },
+  {
+    "revision": "ffb96c4a209f458eda301ec9e19e1551",
+    "url": "./static/js/2.d5cd9cf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d01a1c4f2fb85a7911f",
+    "url": "./static/js/main.2e7b7546.chunk.js"
+  },
+  {
+    "revision": "4bf492bf316ec3539a4e",
+    "url": "./static/js/runtime-main.a24eb5d8.js"
+  },
+  {
+    "revision": "36fe5316b01d8e8fb7bbd47ce0ec6bc2",
+    "url": "./static/media/IMAS.36fe5316.PNG"
+  }
+]);
